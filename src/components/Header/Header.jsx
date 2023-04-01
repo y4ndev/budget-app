fbe7dfa2e20c18bb69fc66d8a9bf1a917d3aa2f8
Yan Golovchenko <y4nweb@gmail.com>
2023-04-01 18:45:46 +0300

    push

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header__title">Budget-App</h1>
+    </div>
+  );
+};
+
+export { Header };
