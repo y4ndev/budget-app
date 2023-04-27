5b5e7ef593ffb8a19a3dc8e88790a9a946c8dfd5
Yan Golovchenko <y4nweb@gmail.com>
2023-04-27 20:48:50 +0300

    push

--- src/components/ExpenseList/ExpenseList.jsx
+++ src/components/ExpenseList/ExpenseList.jsx
@@ -6,20 +6,25 @@ import "./ExpenseList.css";
 const ExpenseList = ({ data, id, active, setActive, onDelete }) => {
   const items = data.filter((item) => item.id === id);
 
+
   return (
     <Modal active={active} setActive={setActive}>
-      <ul className="expense-list" data-id={id}>
-        {items.map(({ expense }) =>
-          expense.map((item, index) => (
-            <li key={index}>
-              <div className="expense__item">
-                {item.name}: {item.price}$
-                <img onClick={() => onDelete(item.id, id)} src={removeImg} alt="remove" />
-              </div>
-            </li>
-          ))
-        )}
-      </ul>
+      {items[0]?.expense?.length ? (
+        <ul className="expense-list" data-id={id}>
+          {items.map(({ expense }) =>
+            expense.map((item, index) => (
+              <li key={index}>
+                <div className="expense__item">
+                  {item.name}: {item.price}$
+                  <img onClick={() => onDelete(item.id, id)} src={removeImg} alt="remove" />
+                </div>
+              </li>
+            ))
+          )}
+        </ul>
+      ) : (
+        <div>Nothing</div>
+      )}
     </Modal>
   );
 };
